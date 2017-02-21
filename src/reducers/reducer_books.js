4e6c55b965f8e62258653efb8c4da3b66e2129b9
Eugene Bredyuk <eugene.bredyuk@gmail.com>
2017-02-21 19:32:29 +0200

    Init commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        { title: 'Kill me please', pages: 203, desc: "This is some description"},
+        { title: 'Redux is fucking harddddddd', pages: 203},
+        { title: 'Wah', pages: 203},
+        { title: 'The last one', pages: 203}
+    ]
+}
