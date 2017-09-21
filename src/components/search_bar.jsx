9ba09899d4e801bf498d5a6c3bcad4cad05fad3e
eugene.bredyuk <eugene.bredyuk@gmail.com>
2017-09-21 23:14:47 +0300

    Added user event to the input
    There's 2 ways - inline and outter function

--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component{
-    render(){
-        return <input type="text"/>;
+    render() {
+        // return <input type="text" onChange={ this.onInputChange }/>;
+        return <input type="text" onChange={ (e) => {console.log(e.target.value);} }/>;
     }
+
+    // onInputChange(e) {
+    //     console.log(e.target.value);
+    // }
 }
 
 export default SearchBar;
