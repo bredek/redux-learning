2fb301dfddbbd8d24b7c218270bd9fffe044e296
eugene.bredyuk <eugene.bredyuk@gmail.com>
2017-09-22 07:38:12 +0300

    Youtube Api to state refactoring

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+// imports
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
@@ -7,20 +8,33 @@ import SearchBar from './components/search_bar';
 
 // consts
 const API_KEY = 'AIzaSyCs-xGMalkiyWH86FwhaLzfaeQWBwZxrrA';
-YTSearch({key:API_KEY, term: 'surfing'}, function(data){
-    console.log(data);
-})
 
-// Create a new component
-const App = () => {
-    return (
-        <div>
-            <h1>Hello!</h1>
-            <p>I've started REACT learning. Again...</p>
+// App component
+class App extends Component {
+    constructor(props) {
+        super(props);
 
-            <SearchBar/>
-        </div>
-    )
+        this.state = {
+            videos: []
+        };
+
+        YTSearch({ key: API_KEY, term: 'surfing' }, (videos) => {
+            this.setState({ videos });
+        })
+
+        console.log(this.state.videos);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Hello!</h1>
+                <p>I've started REACT learning. Again...</p>
+
+                <SearchBar />
+            </div>
+        )
+    }
 }
 // Render component
-ReactDOM.render(<App/>, document.querySelector('.container'));
+ReactDOM.render(<App />, document.querySelector('.container'));
