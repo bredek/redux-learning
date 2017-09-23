ec95dbb90213404add3275e42ac0d5be17160e5e
eugene.bredyuk <eugene.bredyuk@gmail.com>
2017-09-23 06:32:20 +0300

    Extracting info from API

--- src/components/video_list_item.jsx
+++ src/components/video_list_item.jsx
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
 
-const VideoListItem = (props) => {
-    console.log(props.video)
+const VideoListItem = ({video}) => {
     return (
-      <li>Video</li>
+      <li className="list-group-item">
+          <div className="video-list media">
+            <div className="media-left">
+                <img src={video.snippet.thumbnails.default.url} alt="" className="media-object"/>
+            </div>
+            <div className="media-body">
+                <div className="media-heading">
+                    {video.snippet.thumbnails.default.url}
+                </div>
+            </div>
+          </div>
+      </li>
     )
 }
 
--- src/index.jsx
+++ src/index.jsx
@@ -29,12 +29,8 @@ class App extends Component {
     render() {
         return (
             <div>
-                <h1>Hello!</h1>
-                <p>I've started REACT learning. Again...</p>
-
                 <SearchBar />
                 <VideoList videos={this.state.videos} />
-
             </div>
         )
     }
